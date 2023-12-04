f4a367dc5c54f4fba93bae53613709ae3fe50c5c
Emanuel Apaza <emanuel.apaza.200325@gmail.com>
2023-12-04 03:34:04 -0300

    Termino blocking 2da_escena
    Se termino la escena 2 con ayuda de Probuilder y se cargaron materiales para el Player

--- Final_KennyGame/Assets/assets/charactermain/Playercontroller.cs
+++ Final_KennyGame/Assets/assets/charactermain/Playercontroller.cs
@@ -4,82 +4,126 @@ using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.Mathematics;
+using UnityEditor.UI;
 
 public class Playercontroller : MonoBehaviour
 {
     public Animator anim;
     public CharacterController controller;
     public float playerSpeed;
-    private Rigidbody rb;
-    public LayerMask LayerMask;
-    public bool grounded;
+    public Vector3 playervelocity;
+    public bool groundedPLayer;
     public float monedas = 0;
     public canvasmain act;
+
     public Transform camera;
-    public float turnVelocitySmooth=0.1f;
+    public float turnVelocitySmooth = 0.1f;
     public float turnSmoothTime;
+
+    public float jumpheigh;
+    public float gravityValue;
+    public float gravitMutiplay;
+
+    bool isFalling = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     void FixedUpdate() {
+
         Grounded();
+        gravity();
         Jump();
         Move();
+ 
         if (monedas == 10) {
             act.Activarcanvas();
         }
-    }
-    private void Jump() {
-        if (Input.GetKeyDown(KeyCode.Space) && this.grounded) {
-            this.rb.AddForce(Vector3.up * 4, ForceMode.Impulse);
-        }
-    }
-    private void Grounded()
-    {
-        if (Physics.CheckSphere(this.transform.position + Vector3.down, 0.2f, LayerMask))
+        if (!groundedPLayer && playervelocity.y <= 0)
         {
-            this.grounded = true;
+            isFalling = true;
         }
         else
         {
-            this.grounded = false;
+            isFalling = false;
         }
-        this.anim.SetBool("jump", this.grounded);
+
+        // Activa el trigger de caída en el Animator
+        anim.SetBool("isFalling", isFalling);
+    }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && groundedPLayer)
+        {
+            playervelocity.y += Mathf.Sqrt(jumpheigh * -3.0f * gravityValue);
+            anim.SetBool("Jump", true);
+
+            
+        }
+        else
+        {
+            anim.SetBool("Jump", false);
+        }
+
+    }
+    private void gravity() {
+        playervelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playervelocity * Time.deltaTime);
+    }
+    private void Grounded()
+    {
+        groundedPLayer = controller.isGrounded;
+        if (groundedPLayer && playervelocity.y < 0)
+        {
+            playervelocity.y = 0f;
+        }
     }
 
     private void Move()
     {
+
         float verticalAxis = Input.GetAxis("Vertical");
         float horizontalAxis = Input.GetAxis("Horizontal");
-        Vector3 movement = this.transform.forward * verticalAxis + this.transform.right * horizontalAxis;
-        Vector3 move = new Vector3 (horizontalAxis, 0, verticalAxis).normalized;
-        movement.Normalize();
 
-        this.transform.position += movement * 0.04f;
+        Vector3 move = new Vector3 (horizontalAxis,0, verticalAxis).normalized;
         this.anim.SetFloat("SpeedZ", move.z);
         this.anim.SetFloat("SpeedX", move.x);
-        
-        if (move.magnitude >= 0.1f) {
+        this.anim.SetFloat("Magnitude", move.magnitude);
+
+        if (move.magnitude >= 0.1f)
+        {
             float targetAngle;
             Vector3 moveDir;
-            
             //float targetAngle Mathf.Atan2(move.x, nove.z) Mathf.Rad2Deg; //Independiente de la camara.
-            if (Input.GetButton("Fire1")) {
+            if (Input.GetButton("Fire1"))
+            {
                 anim.SetBool("Camfocused", true);
                 targetAngle = camera.eulerAngles.y; //Dependiente de la camara.
-                moveDir = move.z * transform.forward + transform.right* move.x;
+                moveDir = move.z * transform.forward + transform.right * move.x;
             }
-            else {
-                targetAngle = Mathf.Atan2(move.x, move.z)* Mathf.Rad2Deg + camera.eulerAngles.y; //Independiente de la camara, pero con la camara como su forward.
-                moveDir = Quaternion.Euler(0f, targetAngle, 0f)* Vector3.forward;
+            else
+            {
+                targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + camera.eulerAngles.y; //Independiente de la camara, pero con la camara como su forward.
+                moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 anim.SetBool("Camfocused", false);
             }
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocitySmooth, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             controller.Move(moveDir.normalized * Time.deltaTime * playerSpeed);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("Sprinting", true);
+                controller.Move(moveDir.normalized * playerSpeed * 2.7f * Time.deltaTime);
+            }
+            else
+            {
+                anim.SetBool("Sprinting", false);
+            }
         }
     } 
+    
 }
