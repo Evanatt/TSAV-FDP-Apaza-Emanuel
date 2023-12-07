1f197f292376701eea4a0c6598397b803e7c1be2
Emanuel Apaza <emanuel.apaza.200325@gmail.com>
2023-12-07 06:18:20 -0300

    Se completo las condiciones basicas y Avanzadas-y finales
    todo menos la build

---
+++ Final_KennyGame/Assets/Sound/Menu/SonidoMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoMenu : MonoBehaviour {
+    public AudioSource sound;
+    public AudioClip soundMenu;
+    public AudioClip soundMenuselect;
+
+    public void SoundButton()
+    {
+        sound.clip = soundMenu;
+
+        sound.enabled = false;
+        sound.enabled = true;
+    }
+    public void SoundButtonSelect()
+    {
+
+        sound.clip = soundMenuselect;
+
+        sound.enabled = false;
+        sound.enabled = true;
+    }
+
+}
